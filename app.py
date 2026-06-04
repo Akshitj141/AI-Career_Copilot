1dda895a2909571c75c666ec78a2b724d937e483
Akshitj141 <250007424+Akshitj141@users.noreply.github.com>
2026-06-05 02:00:47 +0530

    Initialize project structure and configure  dependencies and environment

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI Resume Analyzer",
+    layout="wide"
+)
+
+st.title("AI Resume Analyzer")
+st.write("Project initialization successful.")
